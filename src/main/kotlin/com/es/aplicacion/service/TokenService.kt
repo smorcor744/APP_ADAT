ec507b87b98b8c2a5826755756339b8d2c0d3d47
Lainezz <diegolinaresmojacar@gmail.com>
2025-02-10 15:49:06 +0100

    adicion de token service

---
+++ src/main/kotlin/com/es/aplicacion/service/TokenService.kt
@@ -0,0 +1,33 @@
+package com.es.aplicacion.service
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.Authentication
+import org.springframework.security.oauth2.jwt.JwtClaimsSet
+import org.springframework.security.oauth2.jwt.JwtEncoder
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters
+import org.springframework.stereotype.Service
+import java.time.Duration
+import java.time.Instant
+import java.util.*
+
+@Service
+class TokenService {
+
+    @Autowired
+    private lateinit var jwtEncoder: JwtEncoder
+
+    fun generarToken(authentication: Authentication) : String {
+
+        val roles: String = authentication.authorities.joinToString(" ") { it.authority } // Contiene los roles del usuario
+
+        val payload: JwtClaimsSet = JwtClaimsSet.builder()
+            .issuer("self")
+            .issuedAt(Instant.now())
+            .expiresAt(Date().toInstant().plus(Duration.ofHours(1)))
+            .subject(authentication.name)
+            .claim("roles", roles)
+            .build()
+
+        return jwtEncoder.encode(JwtEncoderParameters.from(payload)).tokenValue;
+    }
+}
